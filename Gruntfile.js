4824c9dd7920e747ca4d7e477f8d2577789007e2
msqueeg <michaelr.miller@lehmans.com>
2014-04-30 11:40:46 -0400

    working gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,15 +5,11 @@ module.exports = function(grunt) {
   grunt.initConfig({
     // Metadata.
     pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+   
     // Task configuration.
     htmlbuild: {
       dist: {
-        src: 'base/*.html',
+        src: 'base/_test.html',
         dest: 'compile/',
         options: {
           logOptions: true,
@@ -27,57 +23,16 @@ module.exports = function(grunt) {
               footer: 'layout/_footer.html',
               fourColumn: 'layout/_fourCol.html'
             }
-          },
-          data: {
-            version: "0.0.2",
-            title: "ink"
           }
         }
-      }
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        globals: {}
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['lib/**/*.js', 'test/**/*.js']
-      }
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'nodeunit']
       }
     }
   });
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-html-build');
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'htmlbuild']);
+  grunt.registerTask('default', ['htmlbuild']);
 
 };
